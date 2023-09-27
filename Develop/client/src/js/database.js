5f96f0a92f0507e0f3a7524cd54bd8310cd805ee
Niko Auer <nikoauer26@gmail.com>
2023-09-27 11:37:44 +1000

    added put and get requests for db

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,43 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  
+  // connect to desired db 
+  const jateDb = await openDB('jate', 1);
+
+  // creat a transation to the db and its version 
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  // open object store 
+  const store = tx.objectStore('jate');
+
+  // Use put method the pass content to db 
+  const request = store.put({ id: 1, value: content });
+
+  const result = await request;
+  console.log('Data PUT to the database', result);
+
+} 
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+
+  //connect to desired db
+  const jateDb = await openDB('jate', 1);
+
+  //create new transation to readonly
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  // open up object 
+  const store = tx.objectStore('jate');
+
+  //get all the data from the db 
+  const request = store.getAll();
+
+  // get confirmation of results 
+  const result = await request;
+  console.log('Value of results', result);
+};
 
 initdb();
