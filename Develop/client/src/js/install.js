05507c32e8b607a961b89a569b1ad566f3d98731
Niko Auer <nikoauer26@gmail.com>
2023-10-01 11:19:08 +1100

    added comments

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,8 +1,9 @@
 const butInstall = document.getElementById('buttonInstall');
 
 window.addEventListener('beforeinstallprompt', (event) => {
+    // trigger event 
     window.deferredPrompt = event;
-
+    // remove hidden class from buttton 
     butInstall.classList.toggle('hidden', false);
 });
 
@@ -11,8 +12,10 @@ butInstall.addEventListener('click', async () => {
     if (!promptEvent) {
         return;
        }
-            promptEvent.prompt();
+    //    show prompt 
+        promptEvent.prompt();
        
+        // reset variable 
        window.deferredPrompt = null;
        
        butInstall.classList.toggle('hidden', true);
@@ -20,5 +23,6 @@ butInstall.addEventListener('click', async () => {
 
 
 window.addEventListener('appinstalled', (event) => {
+    // rest variable 
     window.deferredPrompt = null;
 });
